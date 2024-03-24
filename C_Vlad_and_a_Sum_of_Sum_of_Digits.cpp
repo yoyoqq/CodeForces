590db91eb6539a851c91141eb7bd2c27bfc3d956
yoyoqq <yagolxuchen@gmail.com>
2024-03-24 12:49:50 +0000

    C vlad and a sum of digits

---
+++ C_Vlad_and_a_Sum_of_Sum_of_Digits.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+ 
+// overload cout and return pair<A, B>
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+// return generic container (vector, set, list) to output stream 
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+// debugging
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+// generate random num
+#define rng_init mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+#define rng_seed(x) mt19937 rng(x)
+// shortcuts
+#define all(x) (x).begin(), (x).end()   // sort(all(a))
+ 
+using ll = long long;
+typedef long double ld;
+const char nl = '\n';
+ 
+const int MX = 200'000 + 1;
+int a[MX];
+ 
+int count_sum_of_digits(int num){
+    int sm = 0;
+    while (num != 0){
+        sm += num % 10;
+        num = num / 10;
+    }
+    return sm;
+}
+ 
+void solve(){
+    // get test cases
+    int n; cin >> n;
+    cout << a[n] << nl;
+    
+}
+ 
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0);
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+ 
+    // precomputation 
+    a[0] = 0;
+    for (int i=1; i<MX; i++){
+        a[i] = count_sum_of_digits(i) + a[i-1];
+    }
+    
+    int test = 1e9; cin >> test;
+    while (test--) solve();
+    return 0;
+}
+ 
+/*
+ 
+other methods:
+brute force for each test case -> too slow 
+    precomputation 
+ 
+time:
+    n * n (precomp)
+    finding o(1)
+ 
+    memory (n)
+ 
+*/
