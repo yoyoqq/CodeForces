4cc77ddd4e38a8100a649d65289ecf613e81e97f
yoyoqq <yagolxuchen@gmail.com>
2024-05-14 15:24:55 +0100

    vlad_and_division

---
+++ D_Vlad_and_Division.cpp
@@ -0,0 +1,63 @@
+
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+// overload cout and return pair<A, B>
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+// return generic container (vector, set, list) to output stream 
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+// debugging
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+// generate random num
+#define rng_init mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+#define rng_seed(x) mt19937 rng(x)
+// shortcuts
+#define all(x) (x).begin(), (x).end()   // sort(all(a))
+
+using ll = long long;
+typedef long double ld;
+const char nl = '\n';
+int A[200010];
+
+void solve(){
+    int n; cin >> n;
+    vector<int> a(n, 0);
+    for (int& x : a) cin >> x;
+    // 
+    // always gonna be either 1 or 2 in each group
+    int group = 0;
+    map<int, int> count;    // store the groups, 1 or 0  
+    for (auto& x : a){
+        // new group
+        if (!count[x]) {
+            // put the reverse in count using xor, 011111111...1111
+            // have all 1, and find the reversed of the x
+            count[((1 << 31) - 1) ^ x]++;
+            group++;
+        }
+        // reset count[x] to 0
+        else count[x]--;
+    }
+    
+    cout << group << nl;
+    
+}
+
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0);
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+    int test = 1e9; cin >> test;
+    while (test--) solve();
+    return 0;
+}
+
+/*
+
+
+
+*/
