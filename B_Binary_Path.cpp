b1f0258a065f964c1e9e7af13708d61921ef5d8e
yoyoqq <yagolxuchen@gmail.com>
2024-05-14 12:37:15 +0100

    binary path

---
+++ B_Binary_Path.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+// overload cout and return pair<A, B>
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+// return generic container (vector, set, list) to output stream 
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+// debugging
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+// generate random num
+#define rng_init mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+#define rng_seed(x) mt19937 rng(x)
+// shortcuts
+#define all(x) (x).begin(), (x).end()   // sort(all(a))
+
+using ll = long long;
+typedef long double ld;
+const char nl = '\n';
+int A[200010];
+
+void solve(){
+    int n; cin >> n;
+    string a, b; cin >> a >> b;
+
+    // get till go down
+    int i = 1;
+    while (i < n && a[i] <= b[i-1])
+        i++;
+    // number of comb 
+    int j = i - 1;
+    while (j > 0 && a[j] == b[j-1])
+        j--;
+    // get res 
+    for (int k=0; k<i; k++) cout << a[k];
+    for (int k=i-1; k<n; k++) cout << b[k];
+    cout << nl;
+    cout << i - j << nl;
+}
+
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0);
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+    int test = 1e9; cin >> test;
+    while (test--) solve();
+    return 0;
+}
+
+/*
+
+
+
+*/
