4657355f5ab1401c6429a075f8356fd9960796b7
yoyoqq <yagolxuchen@gmail.com>
2024-05-23 14:40:18 +0100

    build p

---
+++ C_Build_Permutation.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+// overload cout and return pair<A, B>
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+// return generic container (vector, set, list) to output stream 
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+// debugging
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+// generate random num
+#define rng_init mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+#define rng_seed(x) mt19937 rng(x)
+// shortcuts
+#define all(x) (x).begin(), (x).end()   // sort(all(a))
+
+using ll = long long;
+typedef long double ld;
+const char nl = '\n';
+int N = 1e9;
+vector<int> ans(N);
+
+void rec(int r){
+    if (r < 0) return;
+    int s = sqrt(2 * r);
+    s *= s;
+    int l = s - r; 
+    rec(l-1);
+    for (; l<=r; l++, r--){
+        ans[l] = r;
+        ans[r] = l;
+    }
+}
+
+
+
+void solve(){
+    int n; cin >> n;
+    rec(n-1);
+    for (int i=0; i<n; i++){
+        cout << ans[i] << " ";
+    }
+    cout << nl;
+    
+}
+
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0);
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+    int test = 1e9; cin >> test;
+    while (test--) solve();
+    return 0;
+}
+
+/*
+
+
+
+*/
