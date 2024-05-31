9972454c0059a124b46396b4e09f600ef6d05c25
yoyoqq <yagolxuchen@gmail.com>
2024-05-31 16:29:22 +0100

    fall down

---
+++ G_Fall_Down.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+// overload cout and return pair<A, B>
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+// return generic container (vector, set, list) to output stream 
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+// debugging
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+// generate random num
+#define rng_init mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+#define rng_seed(x) mt19937 rng(x)
+// shortcuts
+#define all(x) (x).begin(), (x).end()   // sort(all(a))
+
+using ll = long long;
+typedef long double ld;
+const char nl = '\n';
+int A[200010];
+
+void solve(){
+    int n, m; cin >> n >> m;
+    char g[n+7][m+7];
+    for (int i=0; i<n; i++){
+        for (int j=0; j<m; j++){
+            cin >> g[i][j];
+        }
+    }
+    // simulate
+    for (int j=0; j<m; j++){
+        // from bottom up 
+        int last = n-1; 
+        for (int i=n-1; i>=0; i--)
+            if (g[i][j] == 'o')
+                last = i-1;
+            else if (g[i][j] == '*'){
+                swap(g[i][j], g[last][j]);
+                last--;
+            }
+    }
+    for (int i=0; i<n; i++){
+        for (int j=0; j<m; j++){
+            cout << g[i][j];
+        }
+        cout << nl;
+    }
+    
+}
+
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0);
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+    int test = 1e9; cin >> test;
+    while (test--) solve();
+    return 0;
+}
+
+/*
+
+
+
+*/
