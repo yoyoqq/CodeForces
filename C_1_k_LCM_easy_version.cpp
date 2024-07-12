3b6a3b76b7fc20d5827a8020cc48abdf1bad8c1d
yoyoqq <39014985+yoyoqq@users.noreply.github.com>
2024-07-12 13:22:58 +0200

    LCM

---
+++ C_1_k_LCM_easy_version.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+// overload cout and return pair<A, B>
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+// return generic container (vector, set, list) to output stream 
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+// debugging
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+// generate random num
+#define rng_init mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+#define rng_seed(x) mt19937 rng(x)
+// shortcuts
+#define all(x) (x).begin(), (x).end()   // sort(all(a))
+
+using ll = long long;
+typedef long double ld;
+const char nl = '\n';
+
+void solve(){
+    int n, k; cin >> n >> k;
+    /*
+        1. n is odd
+        2. n even && != multiple of 4 
+        3. multiple of 4 
+
+    */
+    if (n % 2) cout << 1 << ' ' << n / 2 << ' ' << n / 2 << '\n';
+    else if (n % 2 == 0 && n % 4) cout << 2 << ' ' << n / 2 - 1 << ' ' << n / 2  - 1 << '\n';
+    else cout << n / 2 << ' ' << n / 4 << ' ' << n / 4 << '\n';    
+    
+}
+
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0);
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+    int test = 0; cin >> test;
+    while (test--) solve();
+    return 0;
+}
+
+/*
+
+
+
+*/
