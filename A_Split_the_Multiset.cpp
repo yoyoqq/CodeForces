9cfd3015d9b8f6baa617f2244ab0dc32ecbd34ad
yoyoqq <yagolxuchen@gmail.com>
2024-11-10 12:29:23 +0000

    split to ones

---
+++ A_Split_the_Multiset.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+// overload cout and return pair<A, B>
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+// return generic container (vector, set, list) to output stream 
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+// debugging
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+// generate random num
+#define rng_init mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+#define rng_seed(x) mt19937 rng(x)
+// shortcuts
+#define all(x) (x).begin(), (x).end()   // sort(all(a))
+
+using ll = long long;
+typedef long double ld;
+const char nl = '\n';
+int A[200010];
+
+void solve(){
+    // cout << "HI" << nl;
+    int n, k; cin >> n >> k;
+
+    cout << (n-1 + k-2) / (k-1) << endl;
+    
+    // heap to store the max and take off k from it 
+    // if (n == 1) {cout << 0 << endl; return;}
+    // // while count >= n 
+    // // take from pq
+    // // x -> get k - 1 ones
+    // // push k-1 to pq 
+    // int count = 0;
+    // while (n > 1){
+    //     n = n - k - 1;
+    //     count++;
+    // }
+    // cout << count << endl;
+    
+}
+
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0);
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+    int test = 1e9; cin >> test;
+    while (test--) solve();
+    return 0;
+}
+
+/*
+
+
+
+*/
