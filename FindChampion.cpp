a1d5384d9d6a368377a9a9d3156f953f3e6b2f15
yoyoqq <yagolxuchen@gmail.com>
2024-11-26 14:07:05 +0000

    DAG, use map and check value

---
+++ FindChampion.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int findChampion(int n, vector<vector<int>>& edges) {
+        // DAG
+        // reverse the graph 
+        // iterate n 
+        // if no children is champion
+        //      if more than one chamion return -1
+
+        // get graph reversed pointers
+        unordered_map<int, vector<int>> graph;
+        for (int i=0; i<edges.size(); i++){
+            int x = edges[i][0], y = edges[i][1]; 
+            graph[y].push_back(x);
+        }
+        // find champion
+        int champion = -1; 
+        for (int i=0; i<n; i++){
+            if (graph[i].size() == 0){
+                if (champion != -1){
+                    return -1;
+                }
+                champion = i;
+            }
+        }
+        return champion;
+    }
+};
