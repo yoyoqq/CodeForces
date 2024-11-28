077e2647a730f452a13ddf495a59ba9b9ee4e6d6
yoyoqq <yagolxuchen@gmail.com>
2024-11-28 19:55:45 +0000

    sortings

---
+++ A_Dragons.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+// overload cout and return pair<A, B>
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+// return generic container (vector, set, list) to output stream 
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+// debugging
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+// generate random num
+#define rng_init mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+#define rng_seed(x) mt19937 rng(x)
+// shortcuts
+#define all(x) (x).begin(), (x).end()   // sort(all(a))
+
+using ll = long long;
+typedef long double ld;
+const char nl = '\n';
+int A[200010];
+
+
+
+void solve(){
+    int s, n; cin >> s >> n;
+    vector<pair<int, int>> a(n);
+    int x, y;
+    for (int i=0; i<n; i++){
+        cin >> x >> y;
+        a[i] = {x, y};
+    }
+    // 
+    sort(a.begin(), a.end());
+    bool ans = true;
+    for (int i=0; i<n; i++){
+        if (s <= a[i].first){
+            ans = false;
+            break;
+        }
+        s += a[i].second;
+    }
+    cout << (ans ? "YES" : "NO") << endl;
+    
+
+}
+
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0);
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+    solve();
+    // int test = 1e9; cin >> test;
+    // while (test--) solve();
+    return 0;
+}
+
+/*
+
+1 2
+1 0
+1 0
+
+*/
