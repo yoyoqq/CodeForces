c1781586d18de442da223642f6dcd9e53e72d45d
yoyoqq <yagolxuchen@gmail.com>
2024-12-02 00:29:14 +0000

    string/substr, pattern on equals

---
+++ B_Shohag_Loves_Strings.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+// overload cout and return pair<A, B>
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+// return generic container (vector, set, list) to output stream 
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+// debugging
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+// generate random num
+#define rng_init mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+#define rng_seed(x) mt19937 rng(x)
+// shortcuts
+#define all(x) (x).begin(), (x).end()   // sort(all(a))
+
+using ll = long long;
+typedef long double ld;
+const char nl = '\n';
+int A[200010];
+
+void solve(){
+    string s; cin >> s;
+    if (s.size() == 1){
+        cout << -1 << endl;
+        return;
+    }
+    int n = s.size();
+    // case 1: 2 chars are the same 
+    for (int i=1; i<n; i++){
+        if (s[i] == s[i-1]){
+            cout << s[i] << s[i] << endl;
+            return;
+        }
+    }
+    // case 2: 3 diff chars 
+    for (int i=2; i<n; i++){
+        if (s[i] != s[i-1] && s[i] != s[i-2] && s[i-2] != s[i-1]){
+            cout << s[i-2] << s[i-1] << s[i] << endl;
+            return;
+        }   
+    }
+    cout << -1 << endl;
+    
+}
+
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0);
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+    int test = 1e9; cin >> test;
+    while (test--) solve();
+    return 0;
+}
+
+/*
+
+
+
+*/
