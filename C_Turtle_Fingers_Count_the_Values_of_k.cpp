678c183aecc72a712924726e525d4887654167f5
yoyoqq <yagolxuchen@gmail.com>
2024-12-04 15:38:33 +0000

    a

---
+++ C_Turtle_Fingers_Count_the_Values_of_k.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+// overload cout and return pair<A, B>
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+// return generic container (vector, set, list) to output stream 
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+// debugging
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+// generate random num
+#define rng_init mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+#define rng_seed(x) mt19937 rng(x)
+// shortcuts
+#define all(x) (x).begin(), (x).end()   // sort(all(a))
+
+using ll = long long;
+typedef long double ld;
+const char nl = '\n';
+int A[200010];
+
+void solve(){
+    ll a, b, l; cin >> a >> b >> l;
+    ll count = 0;
+
+    set<int> ans;
+    for (int i=0; i<= 34; i++){
+        int x = l;
+        bool fail = false;
+        for (int _=0; _<i; _++){
+            if (x % a){
+                fail = true;
+                break;
+            }
+            x /= a;
+        }
+        if (fail) break;
+        while (true){
+            ans.insert(x);
+            if (x % b) break;
+            x /= b;
+        }
+    }
+    cout << ans.size() << endl;
+}
+
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0);
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+    int test = 1e9; cin >> test;
+    while (test--) solve();
+    return 0;
+}
+
+/*
+
+
+
+*/
