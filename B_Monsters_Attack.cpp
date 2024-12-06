d560116284326935ea95b9ed48397f73805abb17
yoyoqq <yagolxuchen@gmail.com>
2024-12-06 13:42:08 +0000

    o(n), linear, use remainder and check if >= 0

--- B_Monsters_Attack.cpp
+++ B_Monsters_Attack.cpp
@@ -1,3 +1,74 @@
+
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+// overload cout and return pair<A, B>
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+// return generic container (vector, set, list) to output stream 
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+// debugging
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+// generate random num
+#define rng_init mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+#define rng_seed(x) mt19937 rng(x)
+// shortcuts
+#define all(x) (x).begin(), (x).end()   // sort(all(a))
+
+using ll = long long;
+typedef long double ld;
+const char nl = '\n';
+int A[200010];
+
+void solve(){
+    int n, k; cin >> n >> k;
+    vector<int> a(n), x(n);
+    for(int& u : a) cin >> u;
+    for(int& u : x) cin >> u;
+    vector<int> arr(n+1);
+    for(int i=0; i<n; i++){
+        arr[abs(x[i])] += a[i];
+    }   
+    // dbg_out(arr);
+    // for each step calculate if possible prev bullets + cur bullets  
+    int rem_bullets = 0;
+    bool poss = true;
+    for (int i=1; i<=n; i++){
+        rem_bullets += k - arr[i];
+        poss &= (rem_bullets >= 0);   
+    }
+    cout << (poss ? "YES" : "NO") << endl;
+
+    
+}
+
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0);
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+    int test = 1e9; cin >> test;
+    while (test--) solve();
+    return 0;
+}
+
+/*
+
+
+
+*/
+
+
+
+
+
+
+
+
+
+
 #include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
