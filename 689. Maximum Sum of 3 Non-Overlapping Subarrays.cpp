61b95389a1bff7b4018979a903f008e3cde41dee
yoyoqq <yagolxuchen@gmail.com>
2024-12-28 16:47:20 +0100

    pref, backtrack, dp, dfs

---
+++ 689. Maximum Sum of 3 Non-Overlapping Subarrays.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    vector<int> maxSumOfThreeSubarrays(vector<int>& nums, int k) {
+        // prefix 
+        int n = nums.size() - k + 1;
+        vector<int> sums(n);
+        int windowSum = 0;
+        for (int i=0; i<k; i++){
+            windowSum += nums[i];
+        }
+        sums[0] = windowSum;
+        for (int i=k; i<nums.size(); i++){
+            windowSum = windowSum - nums[i-k] + nums[i];
+            sums[i-k+1] = windowSum;
+        }
+        // dp dfs 
+        vector<vector<int>> memo(n, vector<int>(4, -1));
+        vector<int> indeces;
+        dp(sums, k, 0, 3, memo);
+        dfs(sums, k, 0, 3, memo, indeces);
+        return indeces;
+    }
+
+    int dp(vector<int>& sums, int k, int idx, int rem, vector<vector<int>>& memo){
+        if (rem == 0) return 0;
+        if (idx >= sums.size()){
+            return rem > 0 ? INT_MIN : 0;
+        }
+        if (memo[idx][rem] != -1){
+            return memo[idx][rem];
+        }
+        int cur = sums[idx] + dp(sums, k, idx+k, rem-1, memo);
+        int skip = dp(sums, k, idx+1, rem, memo);
+        memo[idx][rem] = max(cur, skip);
+        return memo[idx][rem];
+    }
+
+    void dfs(vector<int>& sums, int k, int idx, int rem, vector<vector<int>>& memo, vector<int>& indices){
+        if (rem == 0) return;
+        if (idx >= sums.size()) return;
+        int cur = sums[idx] + dp(sums, k, idx+k, rem-1, memo);
+        int skip = dp(sums, k, idx+1, rem, memo);
+        if (cur >= skip){
+            indices.push_back(idx);
+            dfs(sums, k, idx+k, rem-1, memo, indices);
+        }
+        else{
+            dfs(sums, k, idx+1, rem, memo, indices);
+        }
+    }
+};
