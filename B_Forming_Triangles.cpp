13d2703fed92c2bca4ba4b3268988f729052d545
yoyoqq <yagolxuchen@gmail.com>
2024-12-29 14:05:03 +0100

    combinatorics, binomial formula, triangle degenerate

---
+++ B_Forming_Triangles.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+// overload cout and return pair<A, B>
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+// return generic container (vector, set, list) to output stream 
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+// debugging
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+// generate random num
+#define rng_init mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+#define rng_seed(x) mt19937 rng(x)
+// shortcuts
+#define all(x) (x).begin(), (x).end()   // sort(all(a))
+
+using ll = long long;
+typedef long double ld;
+const char nl = '\n';
+int A[200010];
+
+void solve(){
+    int n; cin >> n;
+    map<int, int> freq;
+    for (int i=0, x; i<n; i++){
+        cin >> x;
+        freq[x]++;
+    }   
+    ll res = 0;
+    int sm = 0;
+    for (auto [k, cnt] : freq){
+        // the triangle is equal, binomial coefficient 
+        if (cnt >= 3){
+            res += cnt * (cnt-1) * (cnt-2) / 6;
+        }
+        // two sticks are equal: 
+        if (cnt >= 2){
+            res += cnt * (cnt-1) / 2 * sm;
+        }
+        sm += cnt;
+    }
+    cout << res << endl;
+    
+}
+
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0);
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+    int test = 1e9; cin >> test;
+    while (test--) solve();
+    return 0;
+}
+
+/*
+
+
+
+*/
