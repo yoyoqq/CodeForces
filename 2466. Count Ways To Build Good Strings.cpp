41c1a86167c05765c12d36af742176b0e1445399
yoyoqq <yagolxuchen@gmail.com>
2024-12-30 13:43:29 +0100

    recursive

--- 2466. Count Ways To Build Good Strings.cpp
+++ 2466. Count Ways To Build Good Strings.cpp
@@ -19,3 +19,37 @@ public:
         return ans;
     }
 };
+
+
+
+class Solution {
+public:
+    // backtracking, maths, string manipulation, bit mask 
+    // only need the lenght for each
+    // dp
+
+    const int MOD = 1e9 + 7;
+
+    int countGoodStrings(int low, int high, int zero, int one) {
+        vector<int> values = {low, high, zero, one};
+        map<int, int> memo;
+        return (dp(values, memo, zero) + dp(values, memo, one)) % MOD;
+    }
+
+    // store the values on the arr -> size for each 
+    int dp(vector<int>& values, map<int, int>& memo, int num){
+        int count = 0;
+        if (num > values[1]) return 0;
+        if (memo.find(num) != memo.end()) return memo[num];
+        // valid 
+        if (values[0] <= num && num <= values[1]){
+            count = (count + dp(values, memo, num + values[2]) + dp(values, memo, num + values[3]) + 1) % MOD;
+        }
+        else{
+            count = (count + dp(values, memo, num + values[2])) % MOD;
+            count = (count + dp(values, memo, num + values[3])) % MOD;
+        }
+        memo[num] = count;
+        return count;
+    }
+};
