2c0781c1877d94e209c9e3f12fe80a60043f2995
yoyoqq <yagolxuchen@gmail.com>
2025-02-02 13:58:00 +0000

    greedy

---
+++ D. Even-Odd Game.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+// overload cout and return pair<A, B>
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+// return generic container (vector, set, list) to output stream 
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+// debugging
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+// generate random num
+#define rng_init mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+#define rng_seed(x) mt19937 rng(x)
+// shortcuts
+#define all(x) (x).begin(), (x).end()   // sort(all(a))
+
+using ll = long long;
+typedef long double ld;
+const char nl = '\n';
+int A[200010];
+
+void solve(){
+    int n; cin >> n;
+    vector<int> a(n, 0);
+    for (int& x : a) cin >> x;
+    // 
+    sort(a.begin(), a.end());
+    ll alice = 0, bob = 0;
+    for (int i=n-1; i>= 0; i-=2){
+        if (a[i] % 2 == 0){
+            alice += a[i];
+        }
+    }   
+    for (int i=n-2; i>=0; i-=2){
+        if (a[i] % 2 == 1)
+            bob += a[i];
+    }
+    if (alice == bob){
+        cout << "Tie" << endl;
+    }else if (alice > bob){
+        cout << "Alice" << endl;
+    }else{
+        cout << "Bob" << endl;
+    }
+    
+}
+
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0);
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+    int test = 1e9; cin >> test;
+    while (test--) solve();
+    return 0;
+}
+
+/*
+
+
+
+*/
