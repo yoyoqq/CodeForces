75dc80f085cc267b624c6b2faf5720b83af24628
yoyoqq <yagolxuchen@gmail.com>
2025-02-07 16:19:37 +0000

    make positive

---
+++ A_Diamond_Miner.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+// overload cout and return pair<A, B>
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+// return generic container (vector, set, list) to output stream 
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+// debugging
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+// generate random num
+#define rng_init mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+#define rng_seed(x) mt19937 rng(x)
+// shortcuts
+#define all(x) (x).begin(), (x).end()   // sort(all(a))
+
+using ll = long long;
+typedef long double ld;
+const char nl = '\n';
+int A[200010];
+
+void solve(){
+    int n; cin >> n;
+    vector<int> miner;
+    vector<int> diamond;
+    for (int i=0; i<n*2; i++){
+        int x, y; cin >> x >> y;
+        if (x == 0){
+            miner.push_back(abs(y));
+            // miner.push_back(y);
+        }else{
+            diamond.push_back(abs(x));
+        }
+    }
+    // process output 
+    double ans = 0;
+    sort(all(miner));
+    sort(all(diamond));
+    // dbg_out(miner, diamond);
+    for (int i=0; i<n; i++){
+        ans += sqrt((double)pow(miner[i], 2) + pow(diamond[i], 2));
+        // ans += sqrt(1.0*miner[i]*miner[i] + 1.0*diamond[i]*diamond[i]);
+    }
+    cout << fixed;
+    cout << setprecision(15);
+    cout << ans << endl;
+}
+
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0);
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+    int test = 1e9; cin >> test;
+    while (test--) solve();
+    return 0;
+}
+
+/*
+
+
+
+*/
