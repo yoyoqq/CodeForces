415ea28e1ade8ec8b94a88d48c5275a0f8fedde2
yoyoqq <yagolxuchen@gmail.com>
2025-02-13 15:29:43 +0000

    min max game, max sum, greedy

---
+++ C_1_Magnitude_Easy_Version.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+// overload cout and return pair<A, B>
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+// return generic container (vector, set, list) to output stream 
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream &os, const T_container &v) { os << '{'; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << '}'; }
+// debugging
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+// generate random num
+#define rng_init mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+#define rng_seed(x) mt19937 rng(x)
+// shortcuts
+#define all(x) (x).begin(), (x).end()   // sort(all(a))
+
+using ll = long long;
+typedef long double ld;
+const char nl = '\n';
+ll A[200010];
+
+void solve(){
+    ll n; cin >> n;
+    vector<ll> a(n); for(ll& x  :a)cin >> x;
+    // 
+    // dbg_out(n, a);
+    ll mx = 0;
+    ll mn = 0;
+    for (ll& num : a){
+        ll temp_min = mn;
+        mn = min(mn+num, mx+num);
+        mx = max(mx+num, abs(temp_min+num));
+        // dbg_out(mn, mx);
+    }
+    cout << max(mx, mn) << endl;
+    
+}
+
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0);
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+    ll test = 1e9; cin >> test;
+    while (test--) solve();
+    return 0;
+}
