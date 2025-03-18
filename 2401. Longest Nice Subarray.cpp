80da859f2c8f911f5de5c9d91371f29ac818ea42
yoyoqq <yagolxuchen@gmail.com>
2025-03-18 11:15:49 +0000

    brute force

--- 2401. Longest Nice Subarray.cpp
+++ 2401. Longest Nice Subarray.cpp
@@ -1,6 +1,26 @@
 class Solution {
     public:
         int longestNiceSubarray(vector<int>& nums) {
+            // brute force is max O(30)
+            int n = nums.size(), ans = 0;
+            for (int i=0; i<n; i++){
+                int nice = 0;
+                int count = 0;
+                for (int j=i; j<min(n, i+30); j++){
+                    if ((nice & nums[j]) != 0) break;
+                    nice = nice | nums[j];
+                    count++;
+                }
+                ans = max(ans, count);
+            }
+            return ans;
+        }
+    };
+
+
+class Solution {
+    public:
+        int longestNiceSubarray(vector<int>& nums) {
             // && for all pairs == 0
             // max arr 
             // sliding window -> if !=, xor for left pointer  
