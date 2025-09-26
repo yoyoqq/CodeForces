ad680ca34aa4b4c4a12cc9ef66fd169ab1ec383a
Yagol <39014985+yoyoqq@users.noreply.github.com>
2025-09-26 20:43:43 +0100

    Update 611. Valid Triangle Number.cpp
    cpp

--- 611. Valid Triangle Number.cpp
+++ 611. Valid Triangle Number.cpp
@@ -1,3 +1,19 @@
+class Solution {
+public:
+    int triangleNumber(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int n = nums.size(), ans = 0;
+        for (int i=0; i<n; i++){
+            for (int j=i+1; j<n; j++){
+                int k = (lower_bound(nums.begin(), nums.end(), nums[i]+nums[j]) - nums.begin()) - 1;
+                if (j < k) ans += (k-j);
+            }
+        }
+        return ans;
+    }
+};
+
+
 class Solution:
     def triangleNumber(self, nums: List[int]) -> int:
         nums = sorted(nums)
